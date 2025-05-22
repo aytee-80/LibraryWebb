a2610be7f95fffdaca5f556d28c0ef48137f0da2
aytee-80 <amogelangthompson@gmail.com>
2025-05-22 22:43:49 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Add asadmin to PATH
 export PATH=$GLASSFISH_HOME/bin:$PATH
 
-# Get Render-assigned port or default to 8080
-PORT=${PORT:-8080}
+# Start the domain
+asadmin start-domain domain1
 
-# Start domain in background
-asadmin start-domain domain1 &
-
-# Wait for domain to boot
-sleep 20
-
-# Now update listener config explicitly via localhost:4848
-asadmin --host localhost --port 4848 set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-1.port=$PORT
-asadmin --host localhost --port 4848 set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-1.address=0.0.0.0
-
-# Tail logs to keep container running
+# Tail logs to keep container alive
 tail -f $GLASSFISH_HOME/domains/domain1/logs/server.log
